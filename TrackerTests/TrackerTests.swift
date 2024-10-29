12999935fb06362b1981ffb6e30d6e784c795ea5
Filippov Konstantin <fkv.katafey@yandex.ru>
2024-10-29 13:36:36 +0400

    Initial Commit

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Константин Филиппов on 29.10.2024.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
