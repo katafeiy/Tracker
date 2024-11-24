a6e8260f99dbc69ea711e70e14abd5c25aa6149d
Filippov Konstantin <fkv.katafey@yandex.ru>
2024-11-24 22:00:19 +0400

    Создал отдельный каталог  для хранения  перечислений

--- Models/Tracker.swift
+++ Models/Tracker.swift
@@ -9,57 +9,5 @@ struct Tracker {
     let schedule: Set<DaysOfWeek>
 }
 
-enum DaysOfWeek: Int, CaseIterable {
-    
-    case mon
-    case tue
-    case wen
-    case thu
-    case fri
-    case sat
-    case sun
-    
-    init?(date: Date) {
-        
-        var calendar = Calendar.current
-        calendar.timeZone = .current
-        
-        switch calendar.component(.weekday, from: date) {
-        case 2: self = .mon
-        case 3: self = .tue
-        case 4: self = .wen
-        case 5: self = .thu
-        case 6: self = .fri
-        case 7: self = .sat
-        case 1: self = .sun
-        default: return nil
-        }
-    }
-    
-    var fullName: String {
-        
-        switch self {
-        case .mon: return "Понедельник"
-        case .tue: return "Вторник"
-        case .wen: return "Среда"
-        case .thu: return "Четверг"
-        case .fri: return "Пятница"
-        case .sat: return "Суббота"
-        case .sun: return "Воскресенье"
-        }
-    }
-    
-    var shortName: String {
-        
-        switch self {
-        case .mon: return "Пн"
-        case .tue: return "Вт"
-        case .wen: return "Ср"
-        case .thu: return "Чт"
-        case .fri: return "Пт"
-        case .sat: return "Сб"
-        case .sun: return "Вс"
-        }
-    }
-}
+
 
--- TrackerVC/Habit/NewHabitViewController.swift
+++ TrackerVC/Habit/NewHabitViewController.swift
@@ -418,12 +418,16 @@ extension NewHabitViewController: UITableViewDataSource, UITableViewDelegate {
 extension NewHabitViewController: UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
         guard let textFieldText = textField.text else { return true }
+        
         let newText = (textFieldText as NSString).replacingCharacters(in: range, with: string)
         let count = newText.count < 38 ? newText.count : 38
         let remainingCharacters = 38 - count
+        
         subtitleNameTracker.text = count < 38 ? ("Осталось \(remainingCharacters) символов") : ("Ограничение \(count) символов")
         subtitleNameTracker.textColor = count < 38 ? .ypLightGray : .ypRed
+        
         return newText.count <= 38
     }
     
