a59e8976ada4797d30e6ad7d8458f8b7d9b09f99
Filippov Konstantin <fkv.katafey@yandex.ru>
2025-01-15 00:37:34 +0400

    Добавил скриншот-тесты

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -3,11 +3,26 @@ import SnapshotTesting
 @testable import Tracker
 
 final class TrackerTests: XCTestCase {
-
+    
     func testViewControllerSnapshot() {
         
         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()!
         
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
+    }
+    
+    func testViewController() {
+        let vc = TrackerViewController(viewModel: TrackerViewModel())
+        assertSnapshot(of: vc, as: .image)
+    }
+    
+    func testViewControllerLight() {
+        let vc = TrackerViewController(viewModel: TrackerViewModel())
+        assertSnapshot(of: vc, as: .image(traits: .init(userInterfaceStyle: .light)))
+    }
+    
+    func testViewControllerDark() {
+        let vc = TrackerViewController(viewModel: TrackerViewModel())
+        assertSnapshot(of: vc, as: .image(traits: .init(userInterfaceStyle: .dark)))
     }
 }
